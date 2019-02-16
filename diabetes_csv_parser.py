cd22b436bbe7df37fe02e1572552477427ebc15a
odia101 <odia.akioya@mail.com>
2019-02-16 02:08:48 -0500

    New Script for selected dataset(Diabetes)

--- diabetes_csv_parser.py
+++ diabetes_csv_parser.py
@@ -2,39 +2,32 @@
 
 import os
 import numpy as np
+import pandas as pd
 import csv
-myfilename = "diabetest.txt"
+myfilename = "diabetes.tab.txt"
 
+#  This is Class4 Homework
 
-skip_lines = range(0, 1)	#zero indexed range to skip heading row
-with open(myfilename, 'r') as file_handle1,  open("diabetest1.txt", "r+") as file_handle:	#Creates new file to write data without heading
-	current_line =0  #Keep a line counter
-	for line in file_handle1:	#Read the input file line by line
-		if current_line not in skip_lines:
-			file_handle.write(line)
-		current_line += 1  #To Increase the line counter
-#with open("diabetest1.txt", "r") as file_handle:
+with open(myfilename, 'r') as file_handle:
 	diabeteslist = []
 	for line in file_handle.readlines():
-		line_clean = line.replace('   ', ' ').replace('  ', ' ').replace(',', ' ')
+		line_clean = line.replace('   ', ' ').replace('  ', ' ')
 		line_clean = line_clean.strip()
 		values = line_clean.split('\t')
-#		print(values)
 		caster = []
 		for value in values:
-			try:
-				caster +=[int(value)]
-			except ValueError as e:
-				caster +=[float(value)]
+			if '.' in value:
+				caster.append(float(value))
+			else:
+				caster.append(int(value))
 		diabeteslist += [caster]
-print(diabeteslist[0:5]) #Just printing the first 5 rows
-print("End of list!!!. Now Converting Columns to Rows\n")
-rotated_list = [[diabeteslist[jdx][idx] for jdx, row in enumerate(diabeteslist)] for idx, column in 
+print(diabeteslist[0:2]) #Just printing the first 2 rows
+print("End of list!!!. Now Converting Columns to Rows")
+rotated_list = [[diabeteslist[jdx][idx]for jdx, row in enumerate(diabeteslist)]for idx, column in 
 enumerate(diabeteslist[0])]
 print(rotated_list[0]) #Just showing the first column rotated to row
-           
 
-print('Finished! \nHomework is DONE')
+print('Homework DONE!!!')
 
 
 
